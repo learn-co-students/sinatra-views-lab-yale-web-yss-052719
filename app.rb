a5ecb8850e230e0304c1832a83c5ec6622605ff1
Elijah <efloren@siue.edu>
2019-06-17 14:19:18 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,7 +1,20 @@
 class App < Sinatra::Base
 
-	get '/' do
-		erb :index
+	get '/hello' do
+		status = 200
+		erb :hello
+	end
+
+	get '/goodbye' do
+		status = 200
+		erb :goodbye
+	end
+
+	get '/date' do
+		@date = Date.today.strftime("%A, %B %d, %Y")
+		#binding.pry
+		status = 200
+		erb :date
 	end
 
 
