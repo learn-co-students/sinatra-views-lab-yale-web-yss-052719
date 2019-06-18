4f72c662bc424f4902d7c0dd9704d87c7c466276
github username <github email address>
2019-06-18 17:44:43 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,7 +1,21 @@
+require 'date'
 class App < Sinatra::Base
 
+
 	get '/' do
-		erb :index
+	end
+
+	get '/hello' do
+		erb :hello
+	end
+
+	get '/goodbye' do
+		erb :goodbye
+	end
+
+	get '/date' do
+		@date = Date.today.strftime("%A, %B %d, %Y")
+		erb :date
 	end
 
 
